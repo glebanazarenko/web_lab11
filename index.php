ba9c89d4a45208d1567bbcc0420eb4c19e2f953c
glebanazarenko <106635217+glebanazarenko@users.noreply.github.com>
2022-12-12 20:05:00 +0300

    Добавлена табличная верстка

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Таблица умножения</title>
+    <link rel="stylesheet"  href="style.css">
 </head>
 
 <body>
@@ -59,7 +60,26 @@ else
 // функция ВЫВОДИТ ТАБЛИЦУ УМНОЖЕНИЯ В ТАБЛИЧНОЙ ФОРМЕ
 function outTableForm() 
 { 
-    
+    // если параметр content не был передан в программу
+    if( !isset($_GET['content']) ) 
+        { 
+        echo '<table class=”tdRow”>';
+        for($i=2; $i<10; $i++) // цикл со счетчиком от 2 до 9
+            { 
+            echo '<tr>'; // оформляем таблицу в табличной форме
+            outRowtd( $i ); // вызывем функцию, формирующую содержание
+            // столбца умножения на $i (на 4, если $i==4) 
+            echo '</tr>'; 
+            } 
+            echo '</table>'; 
+        } 
+        
+    else 
+        {
+            echo '<table class=”tdSingleRow”>'; // оформляем таблицу в табличной форме
+            outRowtd( $_GET['content'] ); // выводим выбранный в меню столбец
+            echo '</table>'; 
+        }
 } 
 // функция ВЫВОДИТ ТАБЛИЦУ УМНОЖЕНИЯ В БЛОЧНОЙ ФОРМЕ
 function outDivForm () 
@@ -87,15 +107,35 @@ function outDivForm ()
 // функция ВЫВОДИТ СТОЛБЕЦ ТАБЛИЦЫ УМНОЖЕНИЯ 
 function outRow ( $n ) 
 { 
-    
-    for($i=2; $i<=9; $i++) // цикл со счетчиком от 2 до 9. 
-        echo $n.'x'.$i.'='.($i*$n).'<br>'; // выводим строку столбца с тегом
+    for($i=2; $i<=9; $i++) // цикл со счетчиком от 2 до 9.
+    {
+        if($i*$n <= 9){
+            echo '<a href=?content='.$n.'>'.$n.'</a> x <a href=?content='.$i.'>'.$i.'</a> = <a href=?content='.($i*$n).'>'.($i*$n).'</a><br>'; // выводим строку столбца с тегом
+        }
+        else{
+            echo '<a href=?content='.$n.'>'.$n.'</a> x <a href=?content='.$i.'>'.$i.'</a> = '.($i*$n).'<br>'; // выводим строку столбца с тегом
+        }
+    }
     /*
         echo outNumAsLink($n).'x'.outNumAsLink($i).'='. 
             outNumAsLink($i*$n).'<br>'; 
-            */
-            
+            */       
 } 
+
+
+function outRowtd ( $n )
+{
+    echo'';
+    for($i=2; $i<=9; $i++) // цикл со счетчиком от 2 до 9.
+    {
+        if($i*$n <= 9){
+            echo '<tr><td><a href=?content='.$n.'>'.$n.'</a> * <a href=?content='.$i.'>'.$i.'</a></td><td> <a href=?content='.($i*$n).'>'.($i*$n).'</a></td></tr>';
+        }
+        else{
+            echo '<tr><td><a href=?content='.$n.'>'.$n.'</a> * <a href=?content='.$i.'>'.$i.'</a></td><td>'.($i*$n).'</a></td></tr>';
+        }
+    }
+}
 
 
 ?>
@@ -111,7 +151,7 @@ function outRow ( $n )
         $s.='Таблица умножения полностью. '; 
     else 
         $s.='Столбец таблицы умножения на '.$_GET['content']. '. '; 
-echo $s.date('d.Y.M h:i:s'); ?>
+    echo $s.date('d.Y.M h:i:s'); ?>
     </footer>
 </body>
 
